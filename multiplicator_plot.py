3820304fde756aba10c9ea1b008ef5961b69ad7e
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-04-28 05:49:08 +0300

    Update multiplicator_plot.py

--- multiplicator_plot.py
+++ multiplicator_plot.py
@@ -3,18 +3,19 @@ import csv
 import webbrowser
 x=[]
 y=[]
-
-with open('TimeData1.csv', 'r') as csvfile:
+x1=[]
+y1=[]
+with open('TimeData.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(float(row[0]))
         y.append(float(row[1]))
-
+        x1.append(float(row[0]))
+        y1.append(float(row[2]))
 plt.plot(x,y, marker='', color='green')
-
+plt.plot(x1,y1, marker='', color='indigo')
 plt.title('Algorithm Performance')
-
 plt.xlabel('Number of digits')
 plt.ylabel('Time in milliseconds')
-
 plt.show()
+webbrowser.open('https://www.youtube.com/watch?v=rEoRxWx4FeI')

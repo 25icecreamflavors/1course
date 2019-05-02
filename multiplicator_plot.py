465bdac11000c093a65e34b5c78fa075b8e11a9c
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-05-02 04:15:02 +0300

    Update multiplicator_plot.py

--- multiplicator_plot.py
+++ multiplicator_plot.py
@@ -1,3 +1,4 @@
+    
 import matplotlib.pyplot as plt
 import csv
 import webbrowser
@@ -5,6 +6,7 @@ x=[]
 y=[]
 x1=[]
 y1=[]
+i = 0
 with open('TimeData.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
     for row in plots:
@@ -15,7 +17,12 @@ with open('TimeData.csv', 'r') as csvfile:
 plt.plot(x,y, marker='', color='green')
 plt.plot(x1,y1, marker='', color='indigo')
 plt.title('Algorithm Performance')
+
 plt.xlabel('Number of digits')
 plt.ylabel('Time in milliseconds')
-plt.show()
+print("FLEX")
+print("Would you like to turn on radio?")
+print("Yes/No?")
+i = input()
 webbrowser.open('https://www.youtube.com/watch?v=rEoRxWx4FeI')
+plt.show()

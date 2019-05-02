b3199a95ac96ac2769726c180694f89e633b4ec1
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-05-03 00:57:07 +0300

    Update multiplicator_plot.py

--- multiplicator_plot.py
+++ multiplicator_plot.py
@@ -1,28 +1,35 @@
-    
+import time
+import psutil
 import matplotlib.pyplot as plt
-import csv
+from matplotlib import style
 import webbrowser
+%matplotlib notebook
+
+style.use('classic')
+webbrowser.open('https://www.youtube.com/watch?v=rEoRxWx4FeI')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(1,1,1)
+fig.show()
+
 x=[]
 y=[]
 x1=[]
 y1=[]
-i = 0
+print("Olive line is for School method of multiplication")
+print("Purple line is for Karatsuba method")
 with open('TimeData.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
     for row in plots:
+        ax.clear()
+        ax1.clear()
         x.append(float(row[0]))
         y.append(float(row[1]))
         x1.append(float(row[0]))
         y1.append(float(row[2]))
-plt.plot(x,y, marker='', color='green')
-plt.plot(x1,y1, marker='', color='indigo')
-plt.title('Algorithm Performance')
-
-plt.xlabel('Number of digits')
-plt.ylabel('Time in milliseconds')
-print("FLEX")
-print("Would you like to turn on radio?")
-print("Yes/No?")
-i = input()
-webbrowser.open('https://www.youtube.com/watch?v=rEoRxWx4FeI')
-plt.show()
+        ax.plot(x, y, marker = '', color='indigo')
+        ax1.plot(x1,y1, marker='', color='olive')
+        plt.xlabel('Number of digits')
+        plt.ylabel('Time in milliseconds')
+        plt.title('Algorithm Performance')
+        fig.canvas.draw()

e8ff79fc2bef75bc5f41e10e12f577a6ea786fd8
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-05-03 22:58:38 +0300

    Update multiplicator_plot.py

--- multiplicator_plot.py
+++ multiplicator_plot.py
@@ -1,13 +1,13 @@
 import time
-import csv
 import psutil
+import pylab
+import csv
 import matplotlib.pyplot as plt
 from matplotlib import style
 import webbrowser
 %matplotlib notebook
 
 style.use('classic')
-webbrowser.open('https://www.youtube.com/watch?v=rEoRxWx4FeI')
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax1 = fig.add_subplot(1,1,1)
@@ -17,8 +17,7 @@ x=[]
 y=[]
 x1=[]
 y1=[]
-print("Purple line is for School method of multiplication")
-print("Olive line is for Karatsuba method")
+webbrowser.open('https://www.youtube.com/watch?v=rEoRxWx4FeI')
 with open('TimeData.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
     for row in plots:
@@ -28,10 +27,10 @@ with open('TimeData.csv', 'r') as csvfile:
         y.append(float(row[1]))
         x1.append(float(row[0]))
         y1.append(float(row[2]))
-        ax.clear()
-        ax1.clear()
-        ax.plot(x, y, marker = '', color='indigo')
-        ax1.plot(x1,y1, marker='', color='olive')
+        
+        ax.plot(x, y, marker = '', color='indigo', label='School multiplication')
+        ax1.plot(x1,y1, marker='', color='olive', label='Karatsuba')
+        pylab.legend(loc='upper left')
         plt.xlabel('Number of digits')
         plt.ylabel('Time in milliseconds')
         plt.title('Algorithm Performance')

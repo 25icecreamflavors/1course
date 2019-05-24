04fc69d10c7b40456403bfc5c2a4f07e8145243a
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-05-25 01:49:41 +0300

    Update heapsort.cpp

--- Wii/heapsort.cpp
+++ Wii/heapsort.cpp
@@ -29,3 +29,16 @@ void buildHeap(T& a, size_t size, TPred pred)
     heapify(a, size, i, pred); 
   }
 }
+
+template <typename T, typename TPred>
+void heapsort(T& a, size_t size, TPred pred)
+{
+    buildHeap(a, size, pred);
+
+    for (size_t i = size - 1; i >= 1; i--)
+    {
+        std::swap(a[i], a[0]);
+        size--;
+        heapify(a, size, 0, pred);
+    }
+}

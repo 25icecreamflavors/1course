af455ca9fe1f625bb2a03d53cb2e7435375a5223
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-05-27 21:36:50 +0300

    Update HeapSort.cpp

--- Wii/HeapSort.cpp
+++ Wii/HeapSort.cpp
@@ -3,29 +3,6 @@
 template <typename T, typename TPred>
 void heapify(T& a, size_t size, size_t i, TPred pred)
 {
-  int root = i;
-  int node1 = 2*i + 1;
-  int node2 = 2*i + 2;
-  if ((node1 < size) && (pred(a[node1], a[root]) != true))
-  {
-    root = node1;
-  }
-  if ((node2 < size) && (pred(a[node2], a[root]) != true))
-  {
-    root = node2;
-  }
-  if (root != i)
-  {
-    std::swap(a[i], a[root]);
-    heapify(a, size, root, pred);
-  }
-}
-
-#include <iostream>
-
-template <typename T, typename TPred>
-void heapify(T& a, size_t size, size_t i, TPred pred)
-{
   size_t root = i;
   size_t node1 = (size_t)2*i + (size_t)1;
   size_t node2 = (size_t)2*i + (size_t)2;
@@ -55,4 +32,17 @@ void buildHeap(T& a, size_t size, TPred pred)
     i = i - (size_t)1;
   }
   heapify(a, size, i, pred); 
+}
+
+template <typename T, typename TPred>
+void heapsort(T& a, size_t size, TPred pred)
+{
+    buildHeap(a, size, pred);
+
+    for (size_t i = size - 1; i >= 1; i--)
+    {
+        std::swap(a[i], a[0]);
+        size--;
+        heapify(a, size, 0, pred);
+    }
 }

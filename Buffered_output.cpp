3482699330c65018e2065119be3baae2e3720043
25icecreamflavors <49041568+25icecreamflavors@users.noreply.github.com>
2019-08-07 00:39:47 +0300

    Update Buffered_output.cpp

--- Buffered_output.cpp
+++ Buffered_output.cpp
@@ -1,4 +1,14 @@
-//You have been given the Writer class, which can write data on the screen, in a file or in memory - in general, it doesn't matter where. This class has a virtual function Write, which accepts a pointer to the input bytes of memory and their number. The function tries to write these bytes to the right place. 
+//You have been given the Writer class, which can write data on the screen, 
+//in a file or in memory, in general, it doesn't matter where. 
+//This class has a virtual function Write, which accepts a pointer to the input bytes of memory and their number. 
+//The function tries to write these bytes to the right place. 
+
+//However, the output operation has overhead, and frequent Write calls with small data sizes can slow down the program.
+//You need to write inherited class BufferedWriter that would perform buffered output. 
+//The constructor of this class must take a parameter of type size_t - the size of the buffer.
+//Overriden Write function must first write to the internal buffer, 
+//and only when it is full, call Write of the base class with the data of this buffer. At the end of the usage of the class, 
+//the remaining data in the buffer must also be written through the call to the base class's Write function.
 
 //Class from "writer.h"
 //сlass Writer {
